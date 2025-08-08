3f018a9cd15c907e8dff152583268199a0724ef2
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-08 15:52:53 -0300

    configurando melhor a estrutura de testes

--- app.test.ts
+++ app.test.ts
@@ -2,5 +2,9 @@ import { generateSudoku, sortearNumero, gerarNumeroSemDuplicata } from './app';
 
 
 describe('sortearNumero', () => {
-
+    test('Deve retornar um numero sorteado do array', () => {
+        const numeros = [1, 2, 3, 4];
+        const result = sortearNumero(numeros);
+        expect(numeros).toContain(result)
+    });
 })
